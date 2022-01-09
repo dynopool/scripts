cbb2f9c2cbb8e7c8eef32a7638db06bd12e902c4
dynopool <96736366+dynopool@users.noreply.github.com>
2022-01-09 10:19:30 -0600

    Update node_restart.sh

--- node_restart.sh
+++ node_restart.sh
@@ -1,6 +1,7 @@
 ## This assumes you are using systemd for your stakepool.
 ## systemctl may be used to introspect and control the state of the "systemd" system and service manager.
-## This command will start the systemd cardano node service which will run in the background.
+## This command will reload-or-restart the systemd cardano node service.  
+## The service will run in the background.
 ## Use node_follow.sh to view the node output.
 
 #!/bin/bash
